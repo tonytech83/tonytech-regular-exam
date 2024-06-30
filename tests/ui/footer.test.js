800bce8a722cb74a6aa61a259cfb3d44f0516fa8
tonytech83 <a.petrov.83@gmail.com>
2024-06-30 14:08:30 +0300

    Fix failing UI tests.

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check footer', async ({ page }) => {
     await page.goto('http://localhost:8080');  
-    const footer = await page.$('div');
+    const footer = await page.$('footer > div');
     const text = await footer.textContent();
-    expect(text).toContain('© 2023 - Software Engineering and DevOps Retake Exam.');
+    expect(text).toContain('© 2023 - Software Engineering and DevOps Retake Exam');
   });
   
